e60369e78319fa1c90d9a4afe5303e48550ff081
RyanTarnowski <ryan.tarnowski@gmail.com>
2025-04-21 17:15:43 -0600

    Styling Tweaks

--- src/features/Post/Post.js
+++ src/features/Post/Post.js
@@ -10,9 +10,11 @@ function Post(props) {
             return (
                 <div>
                     {post.comments.map((comment, index) => (
-                        <div key={index}>
-                            <p>Author: {comment.author}</p>
-                            <p>Create Date: {unixToLocalTime(comment.created_utc)}</p>
+                        <div className="Comments" key={index}>
+                            <div className="CommentInfo"> 
+                                <p>Posted by: {comment.author}</p>
+                                <p>Create Date: {unixToLocalTime(comment.created_utc)}</p>
+                            </div>
                             <p>{comment.body}</p>
                         </div>
                     ))}
@@ -24,16 +26,16 @@ function Post(props) {
     }
 
     return (
-        <div className="Post">
-            <h3>{post.title}</h3>
+        <div className="Post">         
             <img src={post.url} alt=''/>
-            <p>Author: {post.author}</p>
-            <p>Create Date: {unixToLocalTime(post.created_utc)}</p>
-
-            <button type="button" onClick={() => onToggleComments(post.permalink, post.showingComments)}>
-                {post.num_comments}
-            </button>
-
+            <h3>{post.title}</h3>
+            <div className="PostInfo"> 
+                <p>Posted by: {post.author}</p>
+                <p>Create Date: {unixToLocalTime(post.created_utc)}</p>
+                <button type="button" onClick={() => onToggleComments(post.permalink, post.showingComments)}>
+                    {post.num_comments}
+                </button>
+            </div>
             {renderComments()}
         </div>
     );
